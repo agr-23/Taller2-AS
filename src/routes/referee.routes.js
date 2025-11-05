fa6022b8accccf3625fd46832256d78b75a834d4
agr <agarcesr@eafit.edu.co>
2025-11-05 11:30:51 -0500

    Links to images

--- src/routes/referee.routes.js
+++ src/routes/referee.routes.js
@@ -11,16 +11,16 @@ router.get('/:id/notifications', ctrl.notifications);
 router.get('/:id/photo', async (req, res) => {
     const { id } = req.params;
     const refereePhotos = {
-        1: 'https://cabapro-referees-images.s3.us-east-1.amazonaws.com/1.jpg',
-        2: 'https://cabapro-referees-images.s3.us-east-1.amazonaws.com/2.jpg',
-        3: 'https://cabapro-referees-images.s3.us-east-1.amazonaws.com/3.jpg',
-        4: 'https://cabapro-referees-images.s3.us-east-1.amazonaws.com/4.jpg',
-        5: 'https://cabapro-referees-images.s3.us-east-1.amazonaws.com/5.jpg',
-        6: 'https://cabapro-referees-images.s3.us-east-1.amazonaws.com/6.jpg',
-        7: 'https://cabapro-referees-images.s3.us-east-1.amazonaws.com/7.jpg',
-        8: 'https://cabapro-referees-images.s3.us-east-1.amazonaws.com/8.jpg',
-        9: 'https://cabapro-referees-images.s3.us-east-1.amazonaws.com/9.jpg',
-        10: 'https://cabapro-referees-images.s3.us-east-1.amazonaws.com/10.jpg',
+        1: 'https://esacosa.s3.us-east-1.amazonaws.com/images/1.jpg',
+        2: 'https://esacosa.s3.us-east-1.amazonaws.com/images/2.jpg',
+        3: 'https://esacosa.s3.us-east-1.amazonaws.com/images/3.jpg',
+        4: 'https://esacosa.s3.us-east-1.amazonaws.com/images/4.jpg',
+        5: 'https://esacosa.s3.us-east-1.amazonaws.com/images/5.jpg',
+        6: 'https://esacosa.s3.us-east-1.amazonaws.com/images/6.jpg',
+        7: 'https://esacosa.s3.us-east-1.amazonaws.com/images/7.jpg',
+        8: 'https://esacosa.s3.us-east-1.amazonaws.com/images/8.jpg',
+        9: 'https://esacosa.s3.us-east-1.amazonaws.com/images/9.jpg',
+        10: 'https://esacosa.s3.us-east-1.amazonaws.com/images/10.jpg',
     };
     const photoUrl = refereePhotos[id];
     if (!photoUrl) return res.status(404).json({ message: 'Referee not found' });
